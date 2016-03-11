7fd2381e499fb18a7370dec8a0105c4c4921206b
Emil Lerch <emil@lerch.org>
2016-03-11 08:56:11 -0800

    add setting of variant bits

--- uuid.go
+++ uuid.go
@@ -11,7 +11,8 @@ func GenerateUUID() string {
 	if _, err := rand.Read(buf); err != nil {
 		panic(fmt.Errorf("failed to read random bytes: %v", err))
 	}
-	buf[6] = (buf[6] & 0x0F) | 0x40 // https://tools.ietf.org/html/rfc4122#section-4.1.3
+	buf[6] = (buf[6] & 0x0F) | 0x40 // Version: https://tools.ietf.org/html/rfc4122#section-4.1.3
+	buf[8] = (buf[8] & 0x3F) | 0x80 // Variant: https://tools.ietf.org/html/rfc4122#section-4.1.1
 
 	return fmt.Sprintf("%08x-%04x-%04x-%04x-%12x",
 		buf[0:4],
